e6e34a2a53bfafbc72ac7384047d2df9b9b627ec
raluca <raluca.pop@e-uvt.ro>
2026-04-18 09:26:11 +0300

    project skeleton

---
+++ backend21/backend21/src/main/java/com/blockchain/backend21/Backend21Application.java
@@ -0,0 +1,13 @@
+package com.blockchain.backend21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend21Application.class, args);
+	}
+
+}
---
+++ backend21/backend21/src/test/java/com/blockchain/backend21/Backend21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockchain.backend21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
